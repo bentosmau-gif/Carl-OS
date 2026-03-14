0e90765d56787d537865bcfc7bebe8408197ddd8
Mau-San <bentosmau@gmail.com>
2026-03-14 11:48:41 -0300

    Update html.c

--- version/chromium/html.c
+++ version/chromium/html.c
@@ -1,2 +1,2 @@
 
-Mozilla/5.0 (Carl OS 1.0; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 CarlBrowser/1.0
+Mozilla/5.0 (Carl OS; x64; rv:119.0) Gecko/20100101 Firefox/119.0 Firefox/119.0
